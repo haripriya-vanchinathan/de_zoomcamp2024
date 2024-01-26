5e725dbf63c76ec9bb123ced03da6953708b31a2
Haripriya Vanchinathan <46278433+haripriya-vanchinathan@users.noreply.github.com>
2024-01-26 05:48:36 +0000

    New message for the combined commit

---
+++ 1_terraform_gcp/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-de-course-412305"
+  region  = "us-central1"
+}
