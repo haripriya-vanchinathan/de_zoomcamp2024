c29aa4439b2ce47d50388c611109c8ee9acdc670
Haripriya Vanchinathan <46278433+haripriya-vanchinathan@users.noreply.github.com>
2024-01-26 06:20:12 +0000

    terraform changes

--- 1_terraform_gcp/main.tf
+++ 1_terraform_gcp/main.tf
@@ -12,3 +12,18 @@ provider "google" {
   project = "terraform-de-course-412305"
   region  = "us-central1"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-de-course-412305-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
